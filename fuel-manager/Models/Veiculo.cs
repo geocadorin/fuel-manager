c5ed0788070faddc556426a4f8c65a93d21d02b8
geocadorin <geeo.cadorin@gmail.com>
2024-03-17 10:44:46 -0300

    Adicionar arquivos de projeto.

---
+++ fuel-manager/Models/Veiculo.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace fuel_manager.Models
+{
+    [Table("Veiculos")]
+    public class Veiculo
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Marca { get; set; }
+        [Required]
+        public string Modelo { get; set; }
+        [Required]
+        public string Placa { get; set; }
+        [Required]
+        public int anoFabricacao { get; set; }
+        [Required]
+        public int anoModelo { get; set; }
+        public ICollection<Consumo> Consumos { get; set; }
+
+    }
+}
